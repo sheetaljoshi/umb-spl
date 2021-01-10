c916c9baa09af38b7fb0b7f6f82e0813a06ec338
sheetal.joshi <sheetal.joshi@moogsoft.com>
2021-01-09 23:28:52 -0500

    I do not nginx index.html

--- provisioning/userdata.sh
+++ provisioning/userdata.sh
@@ -2,6 +2,6 @@
 apt-get update -y
 apt-get upgrade -y
 apt-get install nginx -y
-wget https://raw.githubusercontent.com/nginxinc/NGINX-Demos/master/nginx-hello/index.html --output-document /usr/share/nginx/html/index.html
-wget https://raw.githubusercontent.com/nginxinc/NGINX-Demos/master/nginx-hello/hello.conf --output-document /etc/nginx/sites-enabled/default
+#wget https://raw.githubusercontent.com/nginxinc/NGINX-Demos/master/nginx-hello/index.html --output-document /usr/share/nginx/html/index.html
+#wget https://raw.githubusercontent.com/nginxinc/NGINX-Demos/master/nginx-hello/hello.conf --output-document /etc/nginx/sites-enabled/default
 systemctl restart nginx
